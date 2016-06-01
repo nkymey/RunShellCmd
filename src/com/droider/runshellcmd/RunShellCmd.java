847f7ff63279c40a322fd06f2bb963489f1eeec1
Nick Li <nick.li@wpi-group.com>
2016-06-01 17:54:15 +0800

    first version: Create new android app and build ok, print hello world.

---
+++ src/com/droider/runshellcmd/RunShellCmd.java
@@ -0,0 +1,13 @@
+package com.droider.runshellcmd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RunShellCmd extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_run_shell_cmd);
+	}
+}
